9d03d67a80019445e0cfbf86512bdeffa1b7dd1a
abdullah-25 <imv.abdullah@mail.utoronto.ca>
2026-05-21 17:54:05 -0400

    sol for encoding and decoding using bitmask operator" "

--- ComputerSystems/Bits-and-bytes/encoder.py
+++ ComputerSystems/Bits-and-bytes/encoder.py
@@ -1,39 +1,42 @@
-def encoder(message):
+def encoder(val: int) -> bytes:
     """
-    1 - get binary representation of message
-    2 - add padding of 6 more bits to left
-    3 - break into 2 chunks
-    4 - convert to little endian
-    5 - add continuation bits
-    6 - return
+    150 = 10010110
+    we take 7 smalles bits
+    shift right by 7
+    if not 0 then add 1 to left
+    otherwise break loop
     """
-    binary = ""
-    while message > 0:
-        binary += (str(message % 2))
-        message = message // 2
-    binary = binary[::-1]
-
-    pad_to = 7 * ((len(binary)+6) // 7)
-    binary = binary.zfill(pad_to)
-
-    chunks = [binary[i:i+7] for i in range(0, len(binary), 7)]
-    chunks_rev = []
-    for i in reversed(chunks):
-        chunks_rev.append(i)
-    for i in range(len(chunks_rev)):
-        if i == len(chunks_rev)-1:
-            chunks_rev[i] = "0" + chunks_rev[i]
-        else:
-            chunks_rev[i] = "1" + chunks_rev[i]
-    chunks_rev = [int(chunk, 2) for chunk in chunks_rev]
-    return bytes(chunks_rev)
-
+    out = []
+    while True:
+        # take 7 lowest
+        byte = val & 0b1111111
+        val = val >> 7
+        if val != 0:
+            byte = byte | 0b10000000
+        out.append(byte)
+        if val == 0:
+            break
+    return bytes(out)
 
 def decoder(message):
-    pass
+    n = 0
+    for byte in reversed(message):
+        n = n << 7
+        byte = byte & 0b01111111
+        n = n | byte
+    return n
+    
+ 
 
 if __name__ == "__main__":
     assert encoder(150) == b'\x96\x01'
+    assert encoder(151) == b'\x97\x01'
+    assert encoder(1) == b'\x01'
+    assert decoder(b'\x96\x01') == 150
+    assert decoder(b'\x97\x01') == 151
+    assert decoder(b'\x01') == 1
+    print('ok')
     # print(encoder(150))
     # decoded = decoder(encoded)
     # print(decoded)
+    # print(decoder(b'\x96\x01'))
